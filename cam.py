96e7b4919817ea3a44f7956bbd0607b4b351cb16
Rafael Umetsu <rafael.umetsu@gmail.com>
2023-05-02 21:17:20 -0300

    change cam.py

--- cam.py
+++ cam.py
@@ -1,17 +1,16 @@
+import pandas as pd
 import cv2
 
+url = "https://en.wikipedia.org/wiki/List_of_common_resolutions"
+table = pd.read_html(url)[0]
+table.columns = table.columns.droplevel()
 cap = cv2.VideoCapture(0)
-
 resolutions = {}
-
-for width in range(0, 5000+1, 10):
-    for height in range(0, 5000+1, 10):
-        if 0 in [width, height]:
-            continue
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        resolutions[str(width)+"x"+str(height)] = "OK"
-
+for index, row in table[["W", "H"]].iterrows():
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, row["W"])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, row["H"])
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    resolutions[str(width)+"x"+str(height)] = "OK"
 print(resolutions)
+cap.release()
