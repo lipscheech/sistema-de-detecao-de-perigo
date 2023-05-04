1f9684eef9a52036effcac8dc8d5548beef5d924
Filipe Cavalcante <filipe.khalifa@gmail.com>
2023-05-04 20:14:53 -0300

    corrigir erro

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ flag = Queue(1)
 
 controleMotorThread = Thread(
     target=controleStart,
-    args=(flag)
+    args=([flag])
     )
 
 startInterpreterThread = Thread(
     target=run,
-    args=(
+    args=([
     "fold_0_alpha_0.5_lr_1e-04_regularizer_l1_1e-04_l2_1e-05.tflite",
-    30, 320, 240, 4, flag)
+    30, 320, 240, 4, flag])
     )
 
 controleMotorThread.start()
