61bee3eeb0452bf1d5f7941a2b1f643ac38513e3
Filipe Cavalcante <filipe.khalifa@gmail.com>
2023-05-22 19:06:54 -0300

    subindo testes de controle

--- controleMotor.py
+++ controleMotor.py
@@ -7,6 +7,7 @@
 
 from time import sleep
 import RPi.GPIO as io
+import numpy as np
 
 io.setmode(io.BCM)
 
@@ -15,16 +16,22 @@ io.setwarnings(False)
 L_L_EN = 18 # L_L_EN
 L_R_EN = 17 # L_R_EN
 io.setup(L_L_EN, io.OUT)
-io.output(L_L_EN, True)
 io.setup(L_R_EN, io.OUT)
+io.output(L_L_EN, True)
 io.output(L_R_EN, True)
+
+R_L_EN = 6 # R_L_EN
+R_R_EN = 12 # R_R_EN
+io.setup(R_L_EN, io.OUT)
+io.setup(R_R_EN, io.OUT)
+io.output(R_L_EN, True)
+io.output(R_R_EN, True)
 
 L_L_PWM = 23 # leftmotorpwm_pin_l
 L_R_PWM = 22 # leftmotorpwm_pin_r
 
 io.setup(L_L_PWM, io.OUT)
 io.setup(L_R_PWM, io.OUT)
-
 leftmotorpwm_l = io.PWM(L_L_PWM, 100)
 leftmotorpwm_r = io.PWM(L_R_PWM, 100)
 
@@ -32,13 +39,6 @@ leftmotorpwm_l.start(0)
 leftmotorpwm_r.start(0)
 leftmotorpwm_l.ChangeDutyCycle(0)
 leftmotorpwm_r.ChangeDutyCycle(0)
-
-R_L_EN = 6 # R_L_EN
-R_R_EN = 12 # R_R_EN
-io.setup(R_L_EN, io.OUT)
-io.output(R_L_EN, True)
-io.setup(R_R_EN, io.OUT)
-io.output(R_R_EN, True)
 
 R_L_PWM = 13 # rightmotorpwm_pin_l
 R_R_PWM = 19 # rightmotorpwm_pin_r
@@ -60,6 +60,14 @@ def setMotorLeft(power):
 def setMotorRight(power):
     leftmotorpwm_r.ChangeDutyCycle(power)
     leftmotorpwm_l.ChangeDutyCycle(0)
+
+def over(value, plus):
+   value = value * 100 + plus
+   if value > PWM_MAX:
+       value = PWM_MAX
+   elif value < -PWM_MAX:
+       value = -PWM_MAX
+   return np.round(value* .01, 2)
 
 def exit():
     io.output(L_L_EN, False)
@@ -89,8 +97,8 @@ def controleStart(queueKey=None, flag=None, quit=None):
                 key = "none"
 
             if key == "up":
-                vel_l += int(CHANGE_VALUE * 2)
-                vel_r += int(CHANGE_VALUE * 2)
+                vel_l -= over(vel_l, -CHANGE_VALUE)
+                vel_r += over(vel_r,CHANGE_VALUE)
             elif key == "down":
                 vel_l -= int(CHANGE_VALUE * 2)
                 vel_r -= int(CHANGE_VALUE * 2)
