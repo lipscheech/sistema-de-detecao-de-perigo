f10a3003f0fa1dd2c654394b203c16f3673d05c4
Filipe Cavalcante <filipe.khalifa@gmail.com>
2023-06-03 15:56:59 -0300

    modificando a segmentação

--- interpreter.py
+++ interpreter.py
@@ -66,7 +66,12 @@ def run(PATH: str, FPS: int, imageSize: (int, int), THREAD: int, flag=None, quit
         # INFERENCE
         interpreter.set_tensor(input_index, pre_frame)
         interpreter.invoke()
-        mask = interpreter.get_tensor(output_index)[0].astype(uint8) 
+        mask = interpreter.get_tensor(output_index)[0].astype(uint8)
+
+        cv2.imwrite("images/frame"+str(contSegmentation)+".png", frame)
+        cv2.imwrite("images/mask"+str(contSegmentation)+".png", frame
+        contSegmentation += 1
+
         mask = (mask * attetionArea).sum()
 
         # POSTPROCESS
@@ -85,8 +90,7 @@ def run(PATH: str, FPS: int, imageSize: (int, int), THREAD: int, flag=None, quit
         print(f"fps:{fps} blocked: {flag.is_set()}" )
         # cv2.putText(image, str(fps), (5, 16),
         #             cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 255), 3, cv2.LINE_AA)
-        cv2.imwrite("segmentation"+str(contSegmentation)+"+.png", frame)
-        contSegmentation += 1
+
     print("Ending model loop")
     cap.release()
     cv2.destroyAllWindows()
